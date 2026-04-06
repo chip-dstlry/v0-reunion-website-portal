190da7fe6ef68dd610113640291e1bbfa98ca20b
Chip Mosher <chip@dstlry.co>
2026-04-06 01:26:52 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/v0-reunion-website-portal',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
